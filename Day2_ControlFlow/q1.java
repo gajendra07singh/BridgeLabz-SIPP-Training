fb263222111f47272a98b87946c93881b50a2ec3
gajendra07singh <gs8013059@gmail.com>
2025-06-16 13:41:33 +0530

    New folder and files

---
+++ Day2_ControlFlow/q1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class q1 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int month=sc.nextInt();
+         int day=sc.nextInt();
+           boolean isSpring = (month == 3 && day >= 20) ||
+                           (month == 4) ||
+                           (month == 5) ||
+                           (month == 6 && day <= 20);
+
+         if(isSpring){
+            System.out.println("it is Spring season");
+         }else{
+            System.out.println("its not Spring season");
+         }
+        
+    }
+}
