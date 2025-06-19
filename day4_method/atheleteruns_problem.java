d37f794e53770a94cac6df38ab16eae307353447
gajendra07singh <gs8013059@gmail.com>
2025-06-19 10:57:04 +0530

    day3_method

---
+++ day4_method/atheleteruns_problem.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class atheleteruns_problem {
+    public static double atheleteruns(int side1,int side2,int side3){
+        int perimeter = side1+side2+side3;
+         int distance=5000;
+        int round =distance/perimeter;
+        return round;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int side1=sc.nextInt();
+        int side2=sc.nextInt();
+        int side3=sc.nextInt();
+        double result = atheleteruns(side1,side2,side3);
+        System.out.println("the no of round complete by athelete is "+result);
+
+    }
+}
